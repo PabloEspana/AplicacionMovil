b3b204f28e8e2fdfa50d0ed5c0478fb5b56ea017
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2018-04-10 16:55:10 -0500

    Material design agregado

--- app/src/main/java/com/facci/proyecto/aplicacionmovil/MainActivity.java
+++ app/src/main/java/com/facci/proyecto/aplicacionmovil/MainActivity.java
@@ -1,7 +1,9 @@
 package com.facci.proyecto.aplicacionmovil;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Adding Toolbar to Main screen
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.addTab(tabs.newTab().setText("Chats"));
+        tabs.addTab(tabs.newTab().setText("Contactos"));
+        tabs.addTab(tabs.newTab().setText("Otra opción"));
     }
+
 }

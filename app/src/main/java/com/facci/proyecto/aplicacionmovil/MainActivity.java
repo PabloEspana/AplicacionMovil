cf7b2b381a6d7a2ad967f69c806fedb680ac1363
pablo_cesar1995@hotmail.com <pablo_cesar1995@hotmail.com>
2018-04-11 19:48:32 -0500

    Fragments agregados

--- app/src/main/java/com/facci/proyecto/aplicacionmovil/MainActivity.java
+++ app/src/main/java/com/facci/proyecto/aplicacionmovil/MainActivity.java
@@ -1,25 +1,42 @@
 package com.facci.proyecto.aplicacionmovil;
 
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SectionsPageAdapter sPageAdapter;
+    private ViewPager vP;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+        vP = (ViewPager) findViewById(R.id.viewpager);
+        setupViewPager(vP);
 
         // Adding Toolbar to Main screen
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+        tabs.setupWithViewPager(vP);
+
+        /*TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
         tabs.addTab(tabs.newTab().setText("Chats"));
-        tabs.addTab(tabs.newTab().setText("Contactos"));
-        tabs.addTab(tabs.newTab().setText("Otra opción"));
+        tabs.addTab(tabs.newTab().setText("Grupos"));
+        tabs.addTab(tabs.newTab().setText("Contactos"));*/
+    }
+
+    private void setupViewPager(ViewPager viewPager){
+        SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new contactoFragment(), "Contactos");
+        viewPager.setAdapter(adapter);
     }
 
 }
